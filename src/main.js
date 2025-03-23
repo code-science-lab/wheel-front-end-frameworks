a8f8136132a089a3f3cd14d8aee400883d9c487a
AlanLuo\iteam <iteamalan@hotmail.com>
2025-03-23 22:35:22 +0800

    定向引入静态资源

--- src/main.js
+++ src/main.js
@@ -1,17 +1,3 @@
-// 引入全局样式
-import "./styles/global.css"; // 引入全局样式
-import "./assets/libs/morris.js/morris.css";
-// App css
-import "./assets/css/style.min.css";
-import "./assets/css/icons.min.css";
-// 导入JS
-import $ from "jquery"; // 引入 jquery
-import "jquery-knob"; // 引入 jquery-knob
-// App JS
-import "./assets/js/config";
-import "./assets/js/vendor.min";
-//import "./assets/js/app";
-
 import { Sidebar } from "./components/Sidebar/Sidebar.js"; // 引入 Sidebar 组件
 import { Navbar } from "./components/Navbar/Navbar.js"; // 引入 Navbar 组件
 import { PageTitle } from "./components/PageTitle/PageTitle.js"; // 引入 PageTitle 组件
